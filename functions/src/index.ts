e1ef43129ee1344b01b9d62694d1276a0af4eef0
Lewis Daly <lewisdaly@me.com>
2019-04-09 18:03:13 +0300

    working on handlers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+
+//TODO: make an express handler
+export const getQuote = functions.https.onRequest((request, response) => {
+  
+  const data = [
+    {
+      "paymentMethod": "M-Pesa Mastercard",
+      "fx": 0.0024,
+      "fees": [
+        {
+          "currency": "TSH",
+          "amount": 2000,
+        }
+      ],
+      "sourceTotal": 85245,
+      "sourceCurrency": "TSH",
+      "handoffUrl": "https://url.com",
+    }
+  ];
+
+  response.send(data);
+});
